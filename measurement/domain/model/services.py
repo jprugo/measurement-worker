910ed76d6e20647a3bb01a9add09ca3b152d4601
Juan Pablo Rueda <ruedag.jp@gmail.com>
2024-10-18 11:27:20 -0500

    Update services.py

--- measurement/domain/model/services.py
+++ measurement/domain/model/services.py
@@ -1,36 +1,93 @@
-from typing import Optional
+from sqlalchemy.orm import Session, Query
 
-from sqlalchemy.orm import Session
-
-from measurement.domain.model.aggregate import Measure
-from measurement.domain.model.value_object import MeasureType
-from measurement.infra.repository import MeasurementRepository
-from pydantic import BaseModel
 from datetime import datetime
+from measurement.domain.model.aggregate import Measure, MeasureType
+
+from shared_kernel.infra.database.repository import RDBRepository
+
+from sqlalchemy import func, and_
+from sqlalchemy.orm import aliased
+
+class MeasurementRepository(RDBRepository):
+
+    @staticmethod
+    def find_by_sensor_type_detail_and_date_range(session: Session, measure_type: MeasureType, start_date: datetime, end_date: datetime, detail) -> Query:
+        query = session.query(Measure).filter(
+            Measure.measure_type == measure_type,
+            Measure.created_at >= start_date,
+            Measure.created_at <= end_date,
+        )
+        
+        if detail:
+            query = query.filter(Measure.detail == detail)
+
+        return query.all()
 
 
-class CreateMeasurementRequest(BaseModel):
-    value: float
-    measure_type: MeasureType
-    detail: Optional[str] = None
+    @staticmethod
+    def find_latest_records_for_all_measure_types(session: Session):
+        MeasureAlias = aliased(Measure)
 
+        measure_types = session.query(Measure.measure_type).distinct().all()
 
-class GetMeasurementRequest(BaseModel):
-    measure_type: MeasureType
-    start_date: datetime
-    end_date: datetime
-    detail: Optional[str]
+        latest_records = []
 
+        for measure_type in measure_types:
+            measure_type_value = measure_type[0]
+
+            details = session.query(Measure.detail).filter(
+                Measure.measure_type == measure_type_value
+            ).distinct().all()
+
+            for detail in details:
+                detail_value = detail[0]
+
+                latest_record = session.query(Measure).filter(
+                    and_(
+                        Measure.measure_type == measure_type_value,
+                        Measure.detail == detail_value,
+                        Measure.created_at == session.query(
+                            func.max(MeasureAlias.created_at)
+                        ).filter(
+                            and_(
+                                MeasureAlias.measure_type == measure_type_value,
+                                MeasureAlias.detail == detail_value
+                            )
+                        )
+                    )
+                ).one_or_none()
+
+                if latest_record:
+                    latest_records.append(latest_record)
+
+        return latest_records
     
-class MeasurementService:
-    def __init__(self, repo: MeasurementRepository):
-        self.repo = repo
+    @staticmethod
+    def add(session: Session, instance: Measure):
+        session.add(instance)
+        return instance
+    
+    
+    @staticmethod
+    def commit(session: Session):
+        session.commit()
 
-    def create_measure(self, session: Session, request: CreateMeasurementRequest) -> Measure:
-        measure = Measure.create(
-            value= request.value,
-            measure_type= request.measure_type,
-            detail= request.detail
-        ) 
-        self.repo.add(instance=measure, session=session)
-        return measure
+
+    @staticmethod
+    def delete(session: Session, instance: Measure):
+        session.delete(instance)
+
+
+    @staticmethod
+    def get_by_id(session: Session, entity_class: type, entity_id: int):
+        return session.query(entity_class).get(entity_id)
+    
+
+    @staticmethod
+    def get_all(session: Session, entity_class: type):
+        return session.query(entity_class).all()
+
+
+    @staticmethod
+    def get_by_id(session: Session, entity_class: type, entity_id: int):
+        return session.query(entity_class).get(entity_id)
